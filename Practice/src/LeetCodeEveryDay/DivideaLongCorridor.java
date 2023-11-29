528e728706442f22b494c5c20d737c0337542405
NatarajanThanagaraj <natarajanthanagarajcareer@gmail.com>
2023-11-29 18:39:29 +0530

    Solved

--- Practice/src/LeetCodeEveryDay/DivideaLongCorridor.java
+++ Practice/src/LeetCodeEveryDay/DivideaLongCorridor.java
@@ -5,25 +5,23 @@ import GetInput.Input;
 public class DivideaLongCorridor {
 
 	public static void main(String[] args) {
-		String str ="PP";//Input.getInputString();
+		String str = "SPSPPPSPPSSS";// Input.getInputString();
 		System.out.println(new DivideaLongCorridor().numberOfWays(str));
 	}
 
 	private int numberOfWays(String str) {
-		// char[]arr=str.toCharArray();
-		int ways = 0, sCount = 0,total=1;
-		if (str.length() < 2) {
-			return ways;
-		}
-		for (int i = 0; i < str.length(); i++) {
-			char x = str.charAt(i);
-			if (sCount == 2 ) {
-				ways++;
-			}else {
-				
+		long result=1,seat=0,plant=0;
+		final long MOD=(long)1e9+7;
+		for(int index=0;index<str.length();index++) {
+			char x=str.charAt(index);
+			if(x=='S') {
+				if(++seat>2&&seat%2==1) {
+					result=result*(index-plant)%MOD;
+				}
+				plant=index;
 			}
 		}
-		return total;
+		return (seat>0&&seat%2==0)? (int)result:0;
 	}
 
 }
