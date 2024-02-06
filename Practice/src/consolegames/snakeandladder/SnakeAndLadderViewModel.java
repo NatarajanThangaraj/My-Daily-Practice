2c3e713035653f571cf4922a693aae3adbef3d53
NatarajanThanagaraj <natarajanthanagarajcareer@gmail.com>
2024-02-06 22:29:37 +0530

    update l2

--- Practice/src/consolegames/snakeandladder/SnakeAndLadderViewModel.java
+++ Practice/src/consolegames/snakeandladder/SnakeAndLadderViewModel.java
@@ -9,17 +9,18 @@ import consolegames.snakeandladder.dto.Player;
 
 public class SnakeAndLadderViewModel {
 	SnakeAndLadderView slView;
-	private Cell[][]board;
-	int movesLeftInBoard = 1, nextMoves = 0,destination=board.length*board.length;
+	private Cell[][] board;
 	Queue<Cell> queue = new LinkedList<>();
-	boolean[] visited = new boolean[destination];
+	int movesLeftInBoard = 1, nextMoves = 0, destination;
+	boolean[] visited;
 
-	public SnakeAndLadderViewModel(SnakeAndLadderView snakeAndLadderView,Cell[][] board) {
-		this.board=board;
+	public SnakeAndLadderViewModel(SnakeAndLadderView snakeAndLadderView, Cell[][] board) {
+		this.board = board;
+		this.destination = board.length * board.length;
+		visited = new boolean[destination+1];
 		System.out.println(board);
 		slView = snakeAndLadderView;
-		
-		
+
 	}
 
 	public void checkQuickAction(Cell[][] board, Player player, int result) {
@@ -59,59 +60,53 @@ public class SnakeAndLadderViewModel {
 
 	public int breadthFirstSearch(Cell[][] board) {
 		int moveCount = 0, position = 0;
-		destination= board.length * board.length;
+		destination = board.length * board.length;
 		Cell eachCell = board[board.length - 1][0];
-		boolean win = false;
 		queue.add(eachCell);
 		position = eachCell.getTail();
 		visited[position] = true;
-		while (position < destination) {
+		while (!queue.isEmpty()) {
 			eachCell = queue.remove();
-			position=eachCell.getTail();
+			position = eachCell.getTail();
 			if (position == destination) {
-				win = true;
-				break;
+				return moveCount;
 			}
-			findNextMoves( position);
+			findNextMoves(position);
 			movesLeftInBoard--;
-			if(movesLeftInBoard==0) {
-				movesLeftInBoard=nextMoves;
-				nextMoves=0;
+			if (movesLeftInBoard == 0) {
+				movesLeftInBoard = nextMoves;
+				nextMoves = 0;
 				moveCount++;
 			}
 		}
-		if(win) {
-			return moveCount;
-		}
-return 0;
-		
+		return 0;
+
 	}
 
 	private void findNextMoves(int position) {
 		int currentPosition;
-		for(int i=1;i<7;i++) {
-			currentPosition=position+i;
-			if(currentPosition>destination) 
-				continue;
-			Cell pointingCell=getCell(currentPosition);
-			if(pointingCell.getCellName().equals("#")) {
-				pointingCell=getCell(pointingCell.getTail());
+		for (int i = 1; i < 7; i++) {
+			currentPosition = position + i;
+			if (currentPosition > destination)
+				break;
+			Cell pointingCell = getCell(currentPosition);
+			if (pointingCell.getCellName().equals("#")) {
+				pointingCell = getCell(pointingCell.getTail());
 			}
-			if(pointingCell.getCellName().equals("$")) {
+			if (pointingCell.getCellName().equals("$")) {
 				continue;
 			}
 			queue.add(pointingCell);
-			visited[currentPosition]=true;
+			visited[currentPosition] = true;
 			nextMoves++;
-			
 		}
-		
+
 	}
 
 	private Cell getCell(int currentPosition) {
-		for(int i=board.length-1;i>=0;i--) {
-			for(int j=0;j<board.length;j++) {
-				if(board[i][j].getHead()==currentPosition) {
+		for (int i = board.length - 1; i >= 0; i--) {
+			for (int j = 0; j < board.length; j++) {
+				if (board[i][j].getHead() == currentPosition) {
 					return board[i][j];
 				}
 			}
